d9e046ecb5dd4ae6b82476f7f8953910f760cb8a
Li Boyang <boyangli@email.arizona.edu>
2018-06-15 00:01:51 -0700

    add Heapify.java

--- algorithms/Heapify.java
+++ algorithms/Heapify.java
@@ -6,15 +6,17 @@ public class Solution {
 
     /*
 
-        time = O(nlog(n))
-
+        time = O(n*log(n))
+        
+        shift smaller up
     */
     public void heapify1(int[] A) {
+        // n 
         for(int i = 0 ; i < A.length ; i++){
             shiftUp(A,i);
         }
     }
-    
+    // log(n)
     private void shiftUp(int[] A, int k){
         while(k != 0){
             int father = ( k - 1 ) / 2;
@@ -28,5 +30,39 @@ public class Solution {
             k = father;
         }
     }
+    /*
+        time = O(n)
+        
+        shift larger down
+    */
+     public void heapify2(int[] A) {
+        // n/2
+        for(int i = A.length / 2 ; i >= 0 ; i--){
+            shiftDown(A,i);
+        }
+    }
+    //    log(n/2)
+    private void shiftDown(int[] A, int k){
+        while(k < A.length){
+            int smallIndex = k;
+            // look for child 1
+            if(k * 2 + 1 < A.length && A[k * 2 + 1] < A[smallIndex]){
+                smallIndex = k * 2 + 1;
+            }
+            // look for child 2
+            if(k * 2 + 2 < A.length && A[k * 2 + 2] < A[smallIndex]){
+                smallIndex = k * 2 + 2;
+            }
+            
+            if(smallIndex == k){
+                break;
+            }
+            // shift larger down
+            int temp = A[k];
+            A[k] = A[smallIndex];
+            A[smallIndex] = temp;
+            k = smallIndex;
+        }
+     }
     
 }
